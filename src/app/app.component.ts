fe7e369e5c1a20c55bcf65fbce41ff78423fa267
Anup <anup.code508@gmail.com>
2020-05-25 23:53:14 +0530

    Removed s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to Developers Hub';
+  title = 'Welcome to Developer Hub';
 }
